41ec197a34286f89a2cd019e14ed695b16404265
presto <yoohan95@gmail.com>
2018-11-21 21:55:55 +0900

    [수정] iPad에서 문제 팝업 크기

--- FruitSchool/Exercise/ExerciseContainerViewController.swift
+++ FruitSchool/Exercise/ExerciseContainerViewController.swift
@@ -121,7 +121,7 @@ class ExerciseContainerViewController: UIViewController {
     private func setUp() {
         if deviceModel == .iPad {
             NSLayoutConstraint.activate([
-                containerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.77),
+                containerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
                 containerView.widthAnchor.constraint(equalTo: containerView.heightAnchor, multiplier: 355 / 490)
                 ])
         } else {
