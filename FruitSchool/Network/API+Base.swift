8fb0c675c1d45e493affd2152bab525083e80a21
presto <yoohan95@gmail.com>
2018-11-25 23:55:32 +0900

    [수정] Base URL

--- FruitSchool/Network/API+Base.swift
+++ FruitSchool/Network/API+Base.swift
@@ -14,7 +14,7 @@ import Foundation
  */
 class API {
     //static let baseURL = "http://localhost:3000"
-    static let baseURL = "http://168.62.38.254:3000"
+    static let baseURL = "http://40.76.74.37:3000"
     static let imageBaseURL = "https://fruits2.blob.core.windows.net/fruits/"
     static let jsonDecoder: JSONDecoder = {
         let jsonDecoder = JSONDecoder()
